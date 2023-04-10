b286ff0ccba72387fa454f0213b7fb84ea2bcd04
Avery Schreiner <schreineravery@gmail.com>
2023-04-10 16:58:02 -0500

    part 6

--- controllers/desserts.js
+++ controllers/desserts.js
@@ -19,3 +19,14 @@ res.send('NOT IMPLEMENTED: Dessert delete DELETE ' + req.params.id);
 exports.dessert_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Dessert update PUT' + req.params.id);
 };
+// List of all Costumes
+exports.dessert_list = async function(req, res) {
+    try {
+        theDesserts = await Dessert.find();
+        res.send(theDesserts);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+}
