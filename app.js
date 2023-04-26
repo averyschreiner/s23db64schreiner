469c3961484fbaa9ab6d4f3292aeb50688228f8a
Avery Schreiner <schreineravery@gmail.com>
2023-04-26 13:56:07 -0500

    part 3

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+
 passport.use(new LocalStrategy(
   function(username, password, done) {
-  Account.findOne({ username: username }, function (err, user) {
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
-  });
-}))
+    Account.findOne({ username: username })
+    .then(function (user){
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    })
+    .catch(function(err){
+      return done(err)
+    })
+  })
+)
+
+var Account =require('./models/account')
+passport.use(new LocalStrategy(Account.authenticate()))
+passport.serializeUser(Account.serializeUser())
+passport.deserializeUser(Account.deserializeUser())
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -68,9 +79,9 @@ app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -78,14 +89,6 @@ app.use('/dessert', dessertRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter)
-
-// passport config
-// Use the existing connection
-// The Account model
-var Account =require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Dessert App', user : req.user });
 });
 
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Dessert App Registration'});
+});
+router.post('/register', function(req, res) {
+  Account.findOne({ username : req.body.username })
+  .then(function (user){
+    if(user != null ){
+      console.log("exists " + req.body.username)
+      return res.render('register', { title: 'Registration', message: 'Existing User', account : req.body.username })
+    }
+    let newAccount = new Account({ username : req.body.username });
+    Account.register(newAccount, req.body.password, function(err, user){
+      if (err) {
+        console.log("db creation issue "+ err)
+        return res.render('register', { title: 'Registration', message: 'access error', account : req.body.username })
+      }
+      if(!user){
+        return res.render('register',{ title: 'Registration', message: 'access error', account : req.body.username })
+      }
+    })
+    console.log('Sucess, redirect');
+    res.redirect('/');
+  })
+  .catch(function (err){
+    return res.render('register', { title: 'Registration', message: 'Registration error', account : req.body.username })
+  })
+})
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Dessert App Login', user : req.user })
+})
+router.post('/login', function(req, res) {
+  Account.findOne({ username : req.body.username })
+  .then(function (user){
+    if(user != null ){
+      console.log("login successful " + user)
+      return res.render('index', { user: user})
+    }
+    else {
+      console.log('user not found')
+      return res.render('register', { title: 'No user found. Create an account here'})
+    }
+  })
+  .catch(function (err){
+    return res.render('login', { title: 'Login', message: 'Login error', account : req.body.username })
+  })
+})
 module.exports = router;
